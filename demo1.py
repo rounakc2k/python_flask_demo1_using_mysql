c32bd4971fc36ad2fad8b281dc588d4d90476f66
Rounak Chandrasekhar <55582989+rounakc2k@users.noreply.github.com>
2021-08-02 17:10:27 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Configure db
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = '********'
 app.config['MYSQL_DB'] = 'world'
 
 mysql = MySQL(app)
