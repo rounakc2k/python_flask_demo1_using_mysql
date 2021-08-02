42fbeab39c009ba519e8f5da2127c14f15931c2c
Rounak Chandrasekhar <55582989+rounakc2k@users.noreply.github.com>
2021-08-02 17:09:53 +0530

    Added automatic browser opening

--- demo1.py
+++ demo1.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
-
-app = Flask(__name__)
+import webbrowser
+from threading import Timer
 
 app = Flask(__name__)
 
 # Configure db
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '********'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'world'
 
 mysql = MySQL(app)
@@ -78,8 +78,9 @@ def users1():
         userDetails = cur.fetchall()
         return render_template('users1.html',userDetails=userDetails)
 
+def open_browser():
+      webbrowser.open_new('http://127.0.0.1:5000/')
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+      Timer(1, open_browser).start();
+      app.run(port=5000)
